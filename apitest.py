69856d0de8d38c678affbdb8fa2137c43a4668d3
rashedhasan007 <49535604+rashedhasan007@users.noreply.github.com>
2021-09-14 14:21:43 +0600

    Update apitest.py

--- apitest.py
+++ apitest.py
@@ -3,6 +3,24 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
 def respond():
     # Retrieve the name from url parameter
     name = request.args.get("name", None)
@@ -42,6 +60,10 @@ def post_something():
         })
 
 # A welcome message to test our server
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(books)
+
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
